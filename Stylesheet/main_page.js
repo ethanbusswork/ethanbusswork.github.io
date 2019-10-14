c4686726c0e1108e450cd8516535cc19bbb464a6
Ethan B <ethanbuss.work@gmail.com>
2019-10-14 13:44:51 -0700

    Update to scrolling and animations

--- Stylesheet/main_page.js
+++ Stylesheet/main_page.js
@@ -26,3 +26,29 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+// //The following function adds a smooth scrolling to all links
+
+// $(document).ready(function(){   
+//   $("a").on('click', function(event) {
+	  
+//     // Make sure this.Link has a value before overriding default behavior
+//     if (this.Link !== "") {
+//       // Prevent default anchor click behavior
+//       event.preventDefault();
+
+//       // Store Link
+//       var Link = this.Link;
+
+//       // Using jQuery's animate() method to add smooth page scroll
+//       // The optional number (2000) specifies the number of milliseconds it takes to scroll to the specified area
+//       $('html, body').animate({
+//         scrollTop: $(Link).offset().top
+//       }, 2000, function(){
+   
+//         // Add Link (#) to URL when done scrolling (default click behavior)
+//         window.location.Link = Link;
+//       });
+//     } // End if
+//   });
+// });
